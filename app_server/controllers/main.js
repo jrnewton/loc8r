9567de34d02741c082eaf7b6a855437f908e764e
Jon Newton <rocketnewton@gmail.com>
2020-07-03 15:06:21 -0400

    Stylin' bro

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,5 @@
 const index = (req, res) => { 
+	/* index is index.hbs which is rendered inside of layout.hbs */
   res.render('index', { 
 		pageTitle: 'Hello, Express',
     pageHeader: 'Welcome to Express',
