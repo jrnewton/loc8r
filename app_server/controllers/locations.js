73d00b4628d176cc6e60c3c7308a90696ca8a27a
Jon Newton <rocketnewton@gmail.com>
2020-07-10 17:09:32 -0400

    Prefix page titles

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,14 @@
 const homeList = (req, res) => { 
   /* index is index.hbs which is rendered inside of layout.hbs */
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', { title: 'Loc8r - Home' });
 };
 
 const locationInfo = (req, res) => { 
-  res.render('location-info', { title: 'Location Info' });
+  res.render('location-info', { title: 'Loc8r - Location Info' });
 }
 
 const addReview = (req, res) => { 
-  res.render('location-review', { title: 'Add Review' });
+  res.render('location-review', { title: 'Loc8r - Add Review' });
 }
 
 module.exports = {
