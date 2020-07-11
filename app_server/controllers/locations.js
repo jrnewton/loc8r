43c4b598fdf8e3f2c41dc0cfc01796c88134af4b
Jon Newton <rocketnewton@gmail.com>
2020-07-11 12:18:47 -0400

    Build out a model in the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,39 @@
+
+
+
 const homeList = (req, res) => { 
   /* index is index.hbs which is rendered inside of layout.hbs */
-  res.render('locations-list', { title: 'Loc8r - Home' });
+  res.render('locations-list', {
+      title: 'Loc8r - find a place to work with wifi',
+      pageHeader: {
+        title: 'Loc8r',
+        tagline: 'Find places to work with wifi near you!'
+      }, 
+      locations: [
+        {
+          name: 'Starcups',
+          address: '125 High Street, Reading, RG6 1PS',
+          rating: 3,
+          facilities: ['Hot drinks', 'Food', 'Premium wifi'], 
+          distance: '100m'
+        },
+        {
+          name: 'Cafe Hero',
+          address: '125 High Street, Reading, RG6 1PS',
+          rating: 4,
+          facilities: ['Hot drinks', 'Food', 'Premium wifi'], 
+          distance: '200m'
+        },
+        {
+          name: 'Burger Queen',
+          address: '125 High Street, Reading, RG6 1PS',
+          rating: 2,
+          facilities: ['Hot drinks', 'Food', 'Premium wifi'], 
+          distance: '500m'
+        },
+      ]
+    }
+  );
 };
 
 const locationInfo = (req, res) => { 
