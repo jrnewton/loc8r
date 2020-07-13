8dd2f81a146de3dceb10c1bfc24cc94fedd0fa0f
Jon Newton <rocketnewton@gmail.com>
2020-07-13 17:02:13 -0400

    More data into controller

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,11 @@
 const about = (req, res) => { 
-  res.render('about', { title: 'About' });
+  res.render('about', { 
+    title: 'About', 
+    pageHeader: { 
+      title: 'About', 
+      tagline: 'Loc8r was created to help people find places to sit down and get a bit of work done.'
+    } 
+  });
 }
 
 const test = (req, res) => { 
