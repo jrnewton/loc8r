37309a27430fdd2b3d6161dce484caa943875649
Jon Newton <rocketnewton@gmail.com>
2020-07-14 07:01:16 -0400

    Move model data out of controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,51 +1,4 @@
-const locations = { 
-  1: 
-  {
-    id: 1, 
-    name: 'Starcups',
-    address: '125 High Street, Reading, RG6 1PS',
-    reviewLead: 'Starcups is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-    latLong: '51.455041,-0.9690884',
-    rating: 3,
-    facilities: ['Hot drinks', 'Cold Drinks', 'Premium wifi'], 
-    distance: '100m', 
-    openingHours: [
-      'Monday - Friday : 7:00am - 7:00pm',
-      'Saturday : 8:00am - 5:00pm',
-      'Sunday : closed'
-    ], 
-    reviews: [
-      { 
-        rating: 3, 
-        author: 'Simon Holmes', 
-        date: '16 July 2013', 
-        text: 'What a great place. I can\'t say enough good things about it.'
-      }
-    ]
-  },
-
-  2: 
-  {
-    id: 2, 
-    name: 'Cafe Hero',
-    address: '12 Main Street, Reading, RG6 1PS',
-    rating: 4,
-    facilities: ['Books', 'Food', 'Premium wifi'], 
-    distance: '200m', 
-    openingHours: []
-  },
-
-  3: 
-  {
-    id: 3,
-    name: 'Burger Queen',
-    address: '1875 Lord Tennis Blvd, Reading, RG6 1PS',
-    rating: 2,
-    facilities: ['Food', 'Premium wifi'], 
-    distance: '500m', 
-    openingHours: []
-  }
-}
+const model = require("../model/model.js");
 
 const homeList = (req, res) => { 
   /* locations-list is locations-list.hbs which is rendered inside of layout.hbs */
@@ -58,7 +11,7 @@ const homeList = (req, res) => {
         tagline: 'Find places to work with wifi near you!'
       }, 
       
-      locations: Object.values(locations)
+      locations: Object.values(model.locations)
     }
   );
 };
@@ -66,7 +19,7 @@ const homeList = (req, res) => {
 const locationInfo = (req, res) => { 
   if (req.query.id) { 
     console.log('path=' + req.path + '; query id=' + req.query.id);
-    const location = locations[req.query.id];
+    const location = model.locations[req.query.id];
     res.render('location-info', { 
       title: 'Loc8r - Location Info - ' + location.name, 
       pageHeader: 
