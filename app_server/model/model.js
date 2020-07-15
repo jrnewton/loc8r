97cdf053ef4075039eb1ff509a530d261121b179
Jon Newton <rocketnewton@gmail.com>
2020-07-15 17:04:55 -0400

    Fill in more model data

--- app_server/model/model.js
+++ app_server/model/model.js
@@ -32,7 +32,21 @@ const locations = {
     rating: 4,
     facilities: ['Books', 'Food', 'Premium wifi'], 
     distance: '200m', 
-    openingHours: []
+    openingHours: [
+      'Monday - Friday : 7:00am - 7:00pm',
+      'Saturday : 8:00am - 5:00pm',
+      'Sunday : closed'
+    ], 
+    reviewLead: 'Cafe Hero is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+    latLong: '51.455041,-0.9690884',
+    reviews: [
+      { 
+        rating: 4, 
+        author: 'Dave Jones', 
+        date: '12 Aug 2013', 
+        text: 'Great food and fast wifi!'
+      }
+    ]
   },
 
   3: 
@@ -43,7 +57,21 @@ const locations = {
     rating: 2,
     facilities: ['Food', 'Premium wifi'], 
     distance: '500m', 
-    openingHours: []
+    reviewLead: 'Burger Queen is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+    latLong: '51.455041,-0.9690884',
+    openingHours: [
+      'Monday - Friday : 7:00am - 7:00pm',
+      'Saturday : 8:00am - 5:00pm',
+      'Sunday : closed'
+    ], 
+    reviews: [
+      { 
+        rating: 2, 
+        author: 'Julie Brown',
+        date: '5 August 2015', 
+        text: 'Wifi is so so and seating could be improved.'
+      }
+    ]
   }
 }
 
