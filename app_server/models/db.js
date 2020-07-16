7b04f76025a481223b5846fd728bf3df6c669813
Jon Newton <rocketnewton@gmail.com>
2020-07-15 20:36:20 -0400

    Add shutdown hooks

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const dbURI = 'mongodb://localhost/loc8r';
-
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.on('connected', () => {
@@ -14,4 +13,29 @@ mongoose.connection.on('error', err => {
 
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
+});
+
+const shutdown = (msg, callback) => { 
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+process.once('SIGUSR2', () => {
+  shutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+process.on('SIGINT', () => {
+  shutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', () => {
+  shutdown('app shutdown', () => { 
+    process.exit(0);
+  });
 });
