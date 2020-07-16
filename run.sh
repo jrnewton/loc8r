57b9e7a04bc6a2ece388f473a9d040488b661026
Jon Newton <rocketnewton@gmail.com>
2020-07-15 20:30:15 -0400

    notes on running mongodb

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash 
-# This command was provided by express generator 
+
+#Make sure mongo is running
+#  sudo systemctl status mongod
+#Start it
+#  sudo systemctl start mongod
+
+#Start web server. This command was provided by express generator 
 DEBUG=meanwifi:* npm start
