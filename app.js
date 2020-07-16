fa344e5aa8f166ec248af0c8632faf3a0efb7abd
Jon Newton <rocketnewton@gmail.com>
2020-07-15 20:23:08 -0400

    Initial mongoose setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+require('./app_server/models/db');
 
 hbs.registerHelper('rating', function(count, options) { 
   return count;
---
+++ app_server/models/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://localhost/loc8r';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
