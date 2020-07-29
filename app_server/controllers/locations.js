c1d03921cb1c54736b881274c40af1ecd9406fb0
Jon Newton <rocketnewton@gmail.com>
2020-07-28 23:23:25 -0400

    Undo changes to app_server
    Remove use of DB code.  I'm going to introduce DB code only in the API and then update app_server to use REST.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const model = require("../../app_api/models/static");
-const db = require("../../app_api/models/db");
 
 const homeList = (req, res) => { 
   /* locations-list is locations-list.hbs which is rendered inside of layout.hbs */
@@ -17,32 +16,6 @@ const homeList = (req, res) => {
       locations: model.locations
     }
   );
-};
-
-const locationReadOne = (req, res) => { 
-  console.log(`searching for ${req.params.locationid}`);
-  db.Location
-    .findById(req.params.locationid)
-    .exec( (err, location) => { 
-      if (err) { 
-        return res.status(404).json(err);
-      }
-      else if (!location) { 
-        return res.status(404).json({"message": "location not found"});
-      }
-      else { 
-        res.render('location-info', { 
-          title: 'Loc8r - Location Info - ' + location.name, 
-          pageHeader: 
-          {
-            title: 'Loc8r',
-            tagline: 'Find places to work with wifi near you!'
-          },
-          location: location, 
-          gsm_key: process.env.GSM_KEY
-        });
-      }
-    });
 };
 
 const locationInfo = (req, res) => { 
@@ -80,6 +53,5 @@ const addReview = (req, res) => {
 module.exports = {
   homeList,
   locationInfo,
-  addReview, 
-  locationReadOne
+  addReview
 };
