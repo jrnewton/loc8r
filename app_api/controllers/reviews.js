4094213f14060bccd4310fe69e8829fa6baee7d5
Jon Newton <rocketnewton@gmail.com>
2020-07-28 22:59:23 -0400

    Initial API work
    loc list and loc details working.
    Single review retrieval working.

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,9 +1,50 @@
 'use strict';
 
 /* eslint-disable */
+const db = require("../../app_api/models/db");
 
 const reviewsCreate = (req, res) => { };
-const reviewsReadOne = (req, res) => { };
+const reviewsReadOne = (req, res) => { 
+  let id = req.params.locationid;
+  db.Location
+    .findById(id)
+    //space-separated string of the paths you want to retrieve
+    .select('_id name reviews')
+    .exec( (error, location) => { 
+      //mongoose returns error when bad ID is provided... 
+      if (error || !location) {
+        return res.status(404).json({ 
+          "message": `error retrieving location ${id}`,
+          "error": error
+        });
+      }
+
+      //found it  
+      if (location.reviews && location.reviews.length > 0) { 
+        let reviewId = req.params.reviewid;
+        const review = location.reviews.id(reviewId);
+        if (!review) { 
+          return res.status(404).json({
+            "message": `error retrieving review ${reviewId}`
+          });
+        }
+        else { 
+          return res.status(200).json({
+            location: { 
+              name: location.name, 
+              id: location._id
+            },
+            review
+          });
+        }
+      }
+      else { 
+        return res.status(404).json({
+          "message": `no reviews for location ${id}`
+        });
+      }
+    });
+};
 const reviewsUpdateOne = (req, res) => { };
 const reviewsDeleteOne = (req, res) => { };
 
