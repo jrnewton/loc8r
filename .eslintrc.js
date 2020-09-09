19f74cf67042d14eff949e4ebb19ce54e9a63d78
Jon Newton <rocketnewton@gmail.com>
2020-09-09 08:31:07 -0400

    Require semicolon usage always

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
         "ecmaVersion": 11
     },
     "rules": {
-        "strict": [2, "global"]
+        "strict": "error",
+        "semi": "error"
     },
     "ignorePatterns": [
         "public/javascripts/*.js" /* third party stuff */, 
