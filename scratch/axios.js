dc29dd9889f75a7220fc72d34281ea2f3526062b
Jon Newton <newt@hey.com>
2020-11-15 11:33:12 -0500

    Get with param example

--- scratch/axios.js
+++ scratch/axios.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-(async function test() {
+(async function get() {
   try {
     const response = await axios.get(
       'https://jsonplaceholder.typicode.com/todos/1'
@@ -21,3 +21,20 @@ axios
   .catch((error) => {
     console.error('got an error ', error.message);
   });
+
+(async function getWithParam() {
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts',
+      {
+        params: {
+          userId: 1,
+          id: 1
+        }
+      }
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+})();
