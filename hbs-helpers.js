361ed9eaaa095f7591420c4227016bfd7a4de906
Jon Newton <newt@hey.com>
2020-11-15 21:31:10 -0500

    Turn off debug output now that this code is stable

--- hbs-helpers.js
+++ hbs-helpers.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const debug = require('debug')('meanwifi:hbs-helpers');
+//const debug = require('debug')('meanwifi:hbs-helpers');
 const hbs = require('hbs');
 
 hbs.registerHelper('subtract', function (num1, num2) {
-  debug('subtract: ', num1, num2);
+  //debug('subtract: ', num1, num2);
   return num1 - num2;
 });
 
 hbs.registerHelper('times', function (count, options) {
-  debug('times: ', count);
+  //debug('times: ', count);
   var accum = '';
   for (var i = 0; i < count; i++) accum += options.fn(i);
   return accum;
