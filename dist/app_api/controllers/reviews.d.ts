dbd443fea82123167717b8154efc1448f64f20fa
Jon Newton <newt@hey.com>
2020-11-16 20:37:31 -0500

    d.ts updates

--- dist/app_api/controllers/reviews.d.ts
+++ dist/app_api/controllers/reviews.d.ts
@@ -1,4 +1,4 @@
 export function reviewsCreate(req: any, res: any): any;
 export function reviewsReadOne(req: any, res: any): any;
-export function reviewsUpdateOne(req: any, res: any): void;
-export function reviewsDeleteOne(req: any, res: any): void;
+export function reviewsUpdateOne(req: any, res: any): any;
+export function reviewsDeleteOne(req: any, res: any): any;
